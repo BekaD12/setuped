a71c9a08b63685d7c3c96c5b9714a3356ee50645
Benoit K <28750720+BekaD12@users.noreply.github.com>
2023-08-08 21:30:46 +0200

    Cleanup
    Cleanup for public deploy

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
+const router = useRouter()
 const { t } = useI18n()
 </script>
 
 <template>
-  <div>
-    {{ t('not-found') }}
+  <div class="container-404">
+    <span>{{ t('not-found') }}</span>
+    <button btn @click="router.back()">
+      {{ t('button.back') }}
+    </button>
   </div>
 </template>
+
+<style scoped>
+.container-404 {
+  padding: 4rem;
+  text-align: center;
+}
+
+span {
+  margin-right: 1rem;
+}
+</style>
 
 <route lang="yaml">
 meta:
